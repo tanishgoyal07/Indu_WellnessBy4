9216a850a7c2a353630efe53e4f9aecc4b0df97d
Tanish Goyal <goyaltanish789@gmail.com>
2023-04-15 17:25:11 +0530

    added mythbuster screen with video player

--- lib/screens/map_locator.dart
+++ lib/screens/map_locator.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:indu_wellness/constants.dart';
 
 class MapLocator extends StatefulWidget {
   const MapLocator({super.key});
@@ -10,6 +11,15 @@ class MapLocator extends StatefulWidget {
 class _MapLocatorState extends State<MapLocator> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mainColor,
+        centerTitle: true,
+        title: const Text("Locate"),
+      ),
+      body: const Center(
+        child: Text("Map Page"),
+      ),
+    );
   }
 }
